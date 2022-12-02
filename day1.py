ae497b56f63af1130f5421d1b31c1c82891449e4
petar-savov <p.savov@outlook.com>
2022-12-02 13:15:59 +0000

    Format day 1 Python

--- day1.py
+++ day1.py
@@ -1,10 +1,10 @@
-with open('input1.txt') as f:
+with open("input1.txt") as f:
     calories = [l.strip() for l in f.readlines()]
 
 
 elves = [0]
 for item in calories:
-    if item == '':
+    if item == "":
         elves.append(0)
     else:
         elves[-1] += int(item)
