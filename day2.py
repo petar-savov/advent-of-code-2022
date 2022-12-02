9ccd943aa270fbcc7cf47ef7cf6671572cbeac69
petar-savov <p.savov@outlook.com>
2022-12-02 13:16:10 +0000

    Day 2 Python

---
+++ day2.py
@@ -0,0 +1,29 @@
+with open("input2.txt") as f:
+    lines = [line.strip() for line in f.readlines()]
+
+
+rules1 = {
+    "A X": 3 + 1,
+    "A Y": 6 + 2,
+    "A Z": 0 + 3,
+    "B X": 0 + 1,
+    "B Y": 3 + 2,
+    "B Z": 6 + 3,
+    "C X": 6 + 1,
+    "C Y": 0 + 2,
+    "C Z": 3 + 3,
+}
+rules2 = {
+    "A X": 0 + 3,
+    "A Y": 3 + 1,
+    "A Z": 6 + 2,
+    "B X": 0 + 1,
+    "B Y": 3 + 2,
+    "B Z": 6 + 3,
+    "C X": 0 + 2,
+    "C Y": 3 + 3,
+    "C Z": 6 + 1,
+}
+
+print(sum(rules1[i] for i in lines))
+print(sum(rules2[i] for i in lines))
